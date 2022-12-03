660da4cb854ccba794e3666f8660cc952c0d664b
pgagrwl <pgagrwl@gmail.com>
2022-12-03 20:00:04 +0530

    initial commit

--- deploy/01-deploy-habit.js
+++ deploy/01-deploy-habit.js
@@ -1,62 +1,67 @@
-const { network, ethers } = require("hardhat");
-const {
-  developmentChains,
-  networkConfig,
-} = require("../helper-hardhat-config");
-const { verify } = require("../utils/verify");
+const { network, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
-  const { deploy, log } = deployments;
-  const { deployer } = await getNamedAccounts();
-  const chainId = network.config.chainId;
-  //   let vrfCoordinatorV2Address, subscriptionId;
-  // let habitAddress, subscriptionId;
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    const habitId = ethers.utils.formatBytes32String(networkConfig[chainId]["habitId"])
+    const totalReports = networkConfig[chainId]["totalReports"]
+    const interval = networkConfig[chainId]["interval"]
+    const amount = networkConfig[chainId]["amount"]
 
-  // const habit = await deploy("Habit", {
-  //   from: deployer,
-  //   args: [],
-  //   log: true,
-  //   waitConfirmations: network.config.blockConfirmations || 1,
-  // });
+    //   let vrfCoordinatorV2Address, subscriptionId;
+    // let habitAddress, subscriptionId;
+    if (chainId == 31337) {
+        // create VRFV2 Subscription
+        const habitFactory = await ethers.getContractFactory("Habit")
+        console.log("Deploying contract...")
+        const habit = await habitFactory.deploy()
+        await habit.deployed()
+        console.log(`Deployed contract to: ${habit.address}`)
 
-  const habitFactory = await ethers.getContractFactory("Habit");
-  console.log("Deploying contract...");
-  const habit = await habitFactory.deploy();
-  await habit.deployed();
-  console.log(`Deployed contract to: ${habit.address}`);
-  // what happens when we deploy to our hardhat network?
-  if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
-    console.log("Waiting for block confirmations...");
-    await habit.deployTransaction.wait(6);
-    await verify(habit.address, []);
-  }
+        const transactionResponse = await habit.createHabit(habitId, totalReports, interval, {
+            value: ethers.utils.parseEther(amount),
+        })
+    }
+    // const habit = await deploy("Habit", {
+    //   from: deployer,
+    //   args: [],
+    //   log: true,
+    //   waitConfirmations: network.config.blockConfirmations || 1,
+    // });
 
-  const habitDetails = await habit.getHabit(0);
-  console.log(`Habit detail is: ${habitDetails}`);
+    const habitFactory = await ethers.getContractFactory("Habit")
+    console.log("Deploying contract...")
+    const habit = await habitFactory.deploy()
+    await habit.deployed()
+    console.log(`Deployed contract to: ${habit.address}`)
+    // what happens when we deploy to our hardhat network?
+    // if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+    //     console.log("Waiting for block confirmations...")
+    //     await habit.deployTransaction.wait(6)
+    //     await verify(habit.address, [])
+    // }
 
-  // Update the current value
-  // const transactionResponse = await habit.store(7)
-  const transactionResponse = await habit.createHabit(0, 10, 30, {
-    value: ethers.utils.parseEther("0.01"),
-  });
+    const habitDetails = await habit.getHabit(habitId)
 
-  await transactionResponse.wait(1);
-  // const updatedValue = await habit.retrieve()
-  // console.log(`Updated Value is: ${updatedValue}`)
-  // }
+    console.log(`Habit detail is: ${habitDetails}`)
+    const transactionResponse = await habit.createHabit(habitId, totalReports, interval, {
+        value: ethers.utils.parseEther(amount),
+    })
 
-  const updatedHabitDetail = await habit.getHabit(0);
-  console.log(`Updated Habit detail is: ${updatedHabitDetail}`);
-  // }
+    await transactionResponse.wait(6)
 
-  if (
-    !developmentChains.includes(network.name) &&
-    process.env.ETHERSCAN_API_KEY
-  ) {
-    log("Verifying....");
-    await verify(habit.address);
-  }
-  log("-----------------------------------");
-};
+    const updatedHabitDetail = await habit.getHabit(habitId)
+    console.log(`Updated Habit detail is: ${updatedHabitDetail}`)
+    // }
 
-module.exports.tags = ["all", "habit"];
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying....")
+        await verify(habit.address)
+    }
+    log("-----------------------------------")
+}
+
+module.exports.tags = ["all", "habit"]
