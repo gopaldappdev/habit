c67bc5555fc87cfe2357831b11b53658bfd52257
Keshav Jindal <keshav.j@KESHAV-583-GTG.local>
2022-12-04 03:16:26 +0530

    added send notif func on active habits

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,44 @@
 // Import the required packages
-const {getNotifications} = require("./services/push");
+const cron = require('node-cron');
+const { getNotifications } = require("./services/push");
+const { getActiveHabits } = require("./services/activehabits")
 
-getNotifications();
+
+cron.schedule('*/5 * * * * *', run);
+
+async function run(){
+  console.log("log1");
+  let activeHabits = await getActiveHabits()
+  for(let i=0; i<activeHabits.length; i++){
+    let habit = activeHabits[i]
+    if(toSendNotif(habit) === true){
+      let user = habit.user
+      console.log("log2");
+      getNotifications() // send user in this function
+    }
+  }
+}
+
+function toSendNotif(habit){
+  console.log("log3");
+  const startTime = habit.startTime // 1670076214
+  const interval = habit.interval // 30, in seconds
+  const successCount = habit.successCount
+  const missedCount = habit.missedCount
+
+  let endTimeOfThisReport = startTime + interval * (successCount + missedCount + 1)
+
+  let currentTime = Math.round(Date.now() / 1000)
+  console.log(currentTime);
+
+  if(endTimeOfThisReport - currentTime <= (0.2 * interval)){
+    console.log("log4");
+    return true
+  }
+  else{
+    return false
+  }
+  
+}
+
+// getNotifications();
