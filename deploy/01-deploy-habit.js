7a5ef4d47f4068a39f7884d4e9786912b3212e9c
pgagrwl <pgagrwl@gmail.com>
2022-12-04 07:02:19 +0530

    Updated for hardhat chain

--- deploy/01-deploy-habit.js
+++ deploy/01-deploy-habit.js
@@ -14,34 +14,47 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     const amount = networkConfig[chainId]["amount"]
 
     if (chainId == 31337) {
-        // create VRFV2 Subscription
         const habitFactory = await ethers.getContractFactory("Habit")
         console.log("Deploying contract...")
         const habit = await habitFactory.deploy()
         await habit.deployed()
         console.log(`Deployed contract to: ${habit.address}`)
 
-        const transactionResponse = await habit.createHabit(title, totalReports, interval, {
-            value: ethers.utils.parseEther(amount),
-        })
+        const transactionResponse = await habit.createHabit(
+            title,
+            committed,
+            totalReports,
+            interval,
+            {
+                value: ethers.utils.parseEther(amount),
+            }
+        )
+        await transactionResponse.wait(1)
+        console.log(`Transaction confirmed in ${transactionResponse}`)
+    } else if (chainId != 31337) {
+        const habitFactory = await ethers.getContractFactory("Habit")
+        console.log("Deploying contract to testnet...")
+        const habit = await habitFactory.deploy()
+        await habit.deployed()
+        console.log(`Deployed contract to: ${habit.address}`)
+
+        const transactionResponse = await habit.createHabit(
+            title,
+            committed,
+            totalReports,
+            interval,
+            {
+                value: ethers.utils.parseEther(amount),
+            }
+        )
+        await transactionResponse.wait(6)
+
+        if (!developmentChains.includes(network.name) && process.env.POLYGONSCAN_API_KEY) {
+            log("Verifying....")
+            await verify(habit.address)
+        }
+        log("-----------------------------------")
     }
-
-    const habitFactory = await ethers.getContractFactory("Habit")
-    console.log("Deploying contract to testnet...")
-    const habit = await habitFactory.deploy()
-    await habit.deployed()
-    console.log(`Deployed contract to: ${habit.address}`)
-
-    const transactionResponse = await habit.createHabit(title, committed, totalReports, interval, {
-        value: ethers.utils.parseEther(amount),
-    })
-    await transactionResponse.wait(6)
-
-    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
-        log("Verifying....")
-        await verify(habit.address)
-    }
-    log("-----------------------------------")
 }
 
 module.exports.tags = ["all", "habit"]
